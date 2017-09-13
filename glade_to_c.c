cf2c38d95f45f7f405156b2a17ecff99fcca947e
andi-f <andi_f@vodafonemail.de>
2017-09-13 05:35:26 +0200

    Add files via upload
    RC1

--- glade_to_c.c
+++ glade_to_c.c
@@ -1,5 +1,5 @@
 /* 
-*   A simple program to parse Glade-Files with expat
+*   A simple program to parse Glade-Prototype-Files with expat
 *   and create some c-code.
 *   Parsing the XML:
 *   <http://expat.sourceforge.net/> 
@@ -36,7 +36,7 @@
 #define MAXCHARS 1000000
 #define MAXLEN 1024
 
-#define NODEBUGLEVEL 1
+#define DEBUGLEVEL0
 
 int		widget_level = 0;	//detect a widget
 int		signal_level = 0;	//detect a signal
@@ -48,17 +48,24 @@ FILE	*fp_struct;
     
 char	*filename_widget = "prototyp_widget.c";
 FILE	*fp_widget;
-    
-char	*filename_cb = "prototyp_callback_widget.c";
-FILE	*fp_cb;
 
 char	*filename_widget_h = "prototyp_widget.h";
 FILE	*fp_widget_h;
     
+char	*filename_cb = "prototyp_callback_widget.c";
+FILE	*fp_cb;
+    
 char	*filename_cb_h = "prototyp_callback_widget.h";
 FILE	*fp_cb_h;
+
+char	*filename_sc_h = "prototyp_signal_connect.h";
+FILE	*fp_sc_h;
+
+char	*filename_sc = "prototyp_signal_connect.c";
+FILE	*fp_sc;
     
 char	*filename;
+char	*structname;
 
 void start(void *data, const char *el, const char **attr)	{
 	char 			*str1,*str2;
@@ -66,7 +73,9 @@ void start(void *data, const char *el, const char **attr)	{
 	str1 = NULL;
 	str2 = NULL;	
 
-    //printf("Depth <%i> Element <%s>",Depth,el);
+	#ifdef DEBUGLEVEL2
+		fprintf(stderr, "Depth <%i> Element <%s>",Depth,el);
+    #endif
     
     if(strncmp (el,"object",6) == 0)
 		widget_level = Depth;
@@ -82,19 +91,21 @@ void start(void *data, const char *el, const char **attr)	{
 		
 		if((str1 != NULL) && (str2 != NULL))	{
 			if(strstr(attr[1],"GtkAdjustment") != NULL)	{
-				#ifdef DEBUG
+				#ifdef DEBUGLEVEL1
 					fprintf(stderr, "GtkAdjustment *%s;\n", attr[3]);
 				#endif
 				fprintf(fp_struct, "GtkAdjustment *%s;\n", attr[3]);
 			}
 			else
 			{
-				#ifdef DEBUG
+				#ifdef DEBUGLEVEL1
 					fprintf(stderr, "GtkWidget *%s;\n", attr[3]);		
 				#endif	
-				fprintf(fp_struct, "GtkWidget *%s;\n", attr[3]);			
+				fprintf(fp_struct, "GtkWidget *%s;\n", attr[3]);
+				//GtkAdjustment has no callback
+				fprintf(fp_widget, "\twdg_data->%s = GTK_WIDGET(gtk_builder_get_object(wdg_data->builder, \"%s\"));\n", attr[3], attr[3]);										
 			}
-			fprintf(fp_widget, "\twg_data->%s = GTK_WIDGET(gtk_builder_get_object(wg_data->builder, \"%s\"));\n", attr[3], attr[3]);			
+
 		}
 	}
 
@@ -107,11 +118,22 @@ void start(void *data, const char *el, const char **attr)	{
 		if (attr[2] != NULL)
 			str2 = strstr(attr[2],"handler");		
 		if((str1 != NULL) && (str2 != NULL))	{
-				#ifdef DEBUG			
-					fprintf(stderr, "extern void *%s(GtkWidget *widget, xxxx_data *wdg_data);\n", attr[3]);
+				#ifdef DEBBUGLEVEL1
+					fprintf(stderr, "extern void *%s(GtkWidget *widget, %s_data *wdg_data);\n", attr[3]);
 				#endif
-				fprintf(fp_cb_h, "extern void *%s(GtkWidget *widget, xxxx_data *wdg_data);\n", attr[3]);
-				fprintf(fp_cb, "void *%s(GtkWidget *widget, xxxx_data *wdg_data)\t{\n}\n\n", attr[3]);
+				fprintf(fp_cb_h, "extern void *%s(GtkWidget *widget, %s_data *wdg_data);\n", structname,attr[3]);
+				fprintf(fp_cb, "void %s(GtkWidget *widget, %s_data *wdg_data)\t{\n}\n\n",structname, attr[3]);
+
+				#ifdef DEBBUGLEVEL1
+				fprintf(stderr,"\tif (g_strcmp0 (handler_name, \"%s\") == 0)\n",attr[3]);
+				fprintf(stderr,"\t\tg_signal_connect (object, signal_name, G_CALLBACK(%s), %s_data);\n",attr[3]);
+				fprintf(stderr,"\telse\n");
+				#endif		
+				
+				
+				fprintf(fp_sc,"\tif (g_strcmp0 (handler_name, \"%s\") == 0)\n",attr[3]);
+				fprintf(fp_sc,"\t\tg_signal_connect (object, signal_name, G_CALLBACK(%s), %s_data);\n",attr[3], structname);
+				fprintf(fp_sc,"\telse\n");
 			}
 	}			
   
@@ -132,8 +154,7 @@ end(void *data, const char *el)
 			
 }               /* End of end handler */
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 
     FILE	*fp;
@@ -147,14 +168,22 @@ main(int argc, char **argv)
 	fprintf(stderr, "Glade_to_c\n");
 	fprintf(stderr, "A simple CLI tool to generat some c.code from a Glade file\n");
 	fprintf(stderr, "Written by Andreas Fischer 2017\n");
-	fprintf(stderr, "Version 0.99alpha");
+	fprintf(stderr, "Version 0.99rc1\n");
 	
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s filename [optional widget_structure_name]\n", argv[0]);
         return (-1);
     }
     
-    filename = argv[1];    
+    filename = argv[1];
+    
+    
+    if(strlen(argv[2]) == 0)	{
+			structname = malloc(10);
+			structname = "%s";
+	}
+    else
+		structname = argv[2];
    
 	fprintf(stderr,"reading %s \n\r",argv[1]);
 	
@@ -168,45 +197,56 @@ main(int argc, char **argv)
 		fp_struct = fopen(filename_struct,"w");
 	}	
 	else	{
-	    perror("File for widget structure exists");
-        //exit(EXIT_FAILURE);
-		fp_struct = fopen(filename_struct,"w");        
+	    perror("Prototype-File for widget structure exists");
+		exit(EXIT_FAILURE);
     }    
     
 	if (stat(filename_widget, &sb) == -1) {
 		fp_widget = fopen(filename_widget,"w");
 	}
 	else	{
-	    perror("File for widget function exists");
-        //exit(EXIT_FAILURE);
-		fp_widget = fopen(filename_widget,"w");        
+	    perror("Prototype-File for widget function exists");
+        exit(EXIT_FAILURE);
     }
     
-	if (stat(filename_cb, &sb) == -1) {
-		fp_cb = fopen(filename_cb,"w");
-	}
-	else	{
-	    perror("File for callback function exists");
-        //exit(EXIT_FAILURE);
-        fp_cb = fopen(filename_cb,"w");
-	}
-	
 	if (stat(filename_widget_h, &sb) == -1) {
 		fp_widget_h = fopen(filename_widget_h,"w");
 	}
 	else	{
-	    perror("File for widget function exists");
-        //exit(EXIT_FAILURE);
-		fp_widget_h = fopen(filename_widget_h,"w");        
+	    perror("Prototype-File for widget function exists");
+        exit(EXIT_FAILURE);
     }
-    
+
+	if (stat(filename_cb, &sb) == -1) {
+		fp_cb = fopen(filename_cb,"w");
+	}
+	else	{
+	    perror("Prototype-File for callback function exists");
+        exit(EXIT_FAILURE);
+	}
+	   
 	if (stat(filename_cb_h, &sb) == -1) {
 		fp_cb_h = fopen(filename_cb_h,"w");
 	}
 	else	{
-	    perror("File for callback function exists");
-        //exit(EXIT_FAILURE);
-        fp_cb_h = fopen(filename_cb_h,"w");
+	    perror("Prototype-File for callback function exists");
+        exit(EXIT_FAILURE);
+	}
+
+	if (stat(filename_sc, &sb) == -1) {
+		fp_sc = fopen(filename_sc,"w");
+	}
+	else	{
+	    perror("Prototype-File for callback function exists");
+        exit(EXIT_FAILURE);
+	}
+	   
+	if (stat(filename_sc_h, &sb) == -1) {
+		fp_sc_h = fopen(filename_sc_h,"w");
+	}
+	else	{
+	    perror("Prototype-File for callback function exists");
+        exit(EXIT_FAILURE);
 	}
 	
     if (parser == NULL) {
@@ -220,23 +260,42 @@ main(int argc, char **argv)
 	fprintf(fp_struct,"typedef struct {\n");
 	fprintf(fp_struct,"GtkBuilder *builder;\n\n");
 	
-	fprintf(fp_widget,"#include \"xxxx.h\"\n");
-	fprintf(fp_widget,"#include \"callback_widget.h\"\n\n");
+	fprintf(fp_widget,"#include \"%s.h\"\n",structname);
+	fprintf(fp_widget,"#include \"callback_widget.h\"\n");
+	fprintf(fp_widget,"#include \"signal_connect.h\"\n\n");
+	
+	fprintf(fp_widget,"void wdg_main(%s *wdg_data)\t{\n",structname);
+	fprintf(fp_widget,"\twdg_data->builder = gtk_builder_new();\n");
+    fprintf(fp_widget,"\tgtk_builder_add_from_file (wdg_data->builder, \"%s\", NULL);\n",filename);
+    
+    
+    fprintf(fp_sc_h,"#include <gtk/gtk.h>\n\n");
+	fprintf(fp_sc_h,"extern void connection_mapper (GtkBuilder *builder, GObject *object,\n");
+	fprintf(fp_sc_h,"\tconst gchar *signal_name, const gchar *handler_name,\n");
+	fprintf(fp_sc_h,"\tGObject *connect_object, GConnectFlags flags, gpointer user_data);\n");
 
-	fprintf(fp_widget,"void wdg_main(xxxx_data *wg_data)	{\n");
-	fprintf(fp_widget,"\twg_data->builder = gtk_builder_new();\n");
-    fprintf(fp_widget,"\tgtk_builder_add_from_file (wg_data->builder, %s, NULL);\n",filename);
-    
-    fprintf(fp_cb_h,"#include <gtk/gtk.h>\n\n");
-	fprintf(fp_cb_h,"extern void connection_mapper (GtkBuilder *builder, GObject *object,\n");
-	fprintf(fp_cb_h,"\tconst gchar *signal_name, const gchar *handler_name,\n");
-	fprintf(fp_cb_h,"\tGObject *connect_object, GConnectFlags flags, gpointer user_data);\n");
-    
+    fprintf(fp_sc,"#include <gtk/gtk.h>\n");
+    fprintf(fp_sc,"#include <glib.h>\n");
+	fprintf(fp_sc,"#include <glib/gprintf.h>\n");
+	fprintf(fp_sc,"#include <glib/gprintf.h>\n");
+    fprintf(fp_sc,"#include \"widget-structure.h\"\n\n");
+    fprintf(fp_sc,"void connection_mapper (GtkBuilder *builder, GObject *object,\n");
+	fprintf(fp_sc,"\tconst gchar *signal_name, const gchar *handler_name,\n");
+	fprintf(fp_sc,"\tGObject *connect_object, GConnectFlags flags, %s_data *wdg_data)\t{\n",structname);
+	fprintf(fp_sc,"\tg_print (\"Verbinde %%s mit %%s\\n\", signal_name, handler_name);\n\n");
+	
+	
     /* Tell expat to use functions start() and end() each times it encounters
      * the start or end of an element. */
     XML_SetElementHandler(parser, start, end);
-    
-    fp = fopen(argv[1], "r");
+
+	fp = fopen(argv[1], "r");
+
+	if(fp == NULL)	{
+	    perror("Glade  file not exists");
+        exit(EXIT_FAILURE);
+	}
+
     xmltext = malloc(MAXCHARS);
     
     /* Slurp the XML file in the buffer xmltext */
@@ -251,20 +310,29 @@ main(int argc, char **argv)
     }
     fclose(fp);
 
-    fprintf(fp_struct,"} wdg_data;\n");
+    fprintf(fp_struct,"} %s_data;\n",structname);
     fclose(fp_struct);
 
-	fprintf(fp_widget,"\tgtk_builder_connect_signals_full (wg_data->builder, connection_mapper, (gpointer*) wg_data);\n");
-	fprintf(fp_widget,"\tg_object_unref(wg_data->builder);\n;}\n");
+	fprintf(fp_widget,"\tgtk_builder_connect_signals_full (wdg_data->builder, connection_mapper, (gpointer*) wdg_data);\n");
+	fprintf(fp_widget,"\tg_object_unref(wdg_data->builder);\n;}\n");
     fclose(fp_widget);
     
+	
     fprintf(fp_cb,"}\n");
     fclose(fp_cb);
-    
+
     fprintf(fp_cb_h,"\n");
     fclose(fp_cb_h);
     
-    fprintf(fp_widget_h,"extern void wdg_main(xxxx_data *wg_data);");
+    fprintf(fp_sc,"\t\tg_print (\"unknown callback\\n\");\n");
+    fprintf(fp_sc,"}\n");
+    fclose(fp_sc);
+
+    fprintf(fp_sc_h,"\n");
+    fclose(fp_sc_h);
+
+
+    fprintf(fp_widget_h,"extern void wdg_main(xxx_data *wdg_data);");
     fclose(fp_widget_h);
     
     XML_ParserFree(parser);
